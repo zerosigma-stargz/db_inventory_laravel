570ee44be2a48f14a1eb49493b6f728f6f108a1c
zerosigma-stargz <ardiyantorifky927@gmail.com>
2026-05-21 12:08:19 +0700

    Sistem Manajemen Inventori Gudang

---
+++ db_inventori-app/README.md
@@ -0,0 +1,3 @@
+Membuat sistem Manajemen Inventori Gudang mengggunakan Laravel.
+Sistem memiliki fitur authentification, CRUD master data, stock transaction, validasi, 
+upload image, pagination, serta REST API respons standard.
---
+++ db_inventori-app/app/Models/User.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Database\Factories\UserFactory;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Attributes\Hidden;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Fortify\Contracts\PasskeyUser;
+use Laravel\Fortify\PasskeyAuthenticatable;
+use Laravel\Fortify\TwoFactorAuthenticatable;
+
+#[Fillable(['name', 'email', 'password'])]
+#[Hidden(['password', 'two_factor_secret', 'two_factor_recovery_codes', 'remember_token'])]
+class User extends Authenticatable implements PasskeyUser
+{
+    /** @use HasFactory<UserFactory> */
+    use HasFactory, Notifiable, PasskeyAuthenticatable, TwoFactorAuthenticatable;
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+            /* @chisel-2fa */
+            'two_factor_confirmed_at' => 'datetime',
+            /* @end-chisel-2fa */
+        ];
+    }
+}
